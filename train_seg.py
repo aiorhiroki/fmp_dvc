5deaefb163c9dec04f251cc47601b196731e9ee1
hiroki <1234defgsigeru@gmail.com>
2022-04-21 00:07:53 +0900

    main function

--- train_seg.py
+++ train_seg.py
@@ -5,10 +5,6 @@ import torch
 
 
 class AnnotationImp(fmp.GetAnnotationABC):
-    target = "./seg_data/CamVid"
-    get_train_fn = fmp.readers.CaseDirect("train", "trainannot")
-    get_val_fn = fmp.readers.CaseDirect("val", "valannot")
-
     """
     def __call__(self):
         # you can override GetAnnotation function
@@ -17,13 +13,6 @@ class AnnotationImp(fmp.GetAnnotationABC):
 
 
 class DatasetImp(fmp.GetDatasetSgmABC):
-    class_values = [1, 8]
-    train_trans = albu.Compose(
-        [albu.augmentations.geometric.resize.Resize(256, 512),
-         albu.augmentations.transforms.HorizontalFlip(p=0.5)])
-    val_trans = albu.Compose(
-        [albu.augmentations.geometric.resize.Resize(256, 512)])
-
     """
     def preprocess(self, image, mask):
         # set custom preprocessing function
@@ -39,6 +28,22 @@ class DatasetImp(fmp.GetDatasetSgmABC):
 
 
 class OptimizationImp(fmp.GetOptimizationABC):
+    @staticmethod
+    def scheduler_func(epoch):
+        return 0.95 ** (epoch-10) if epoch > 10 else 1
+
+
+def main():
+    target = "./seg_data/CamVid"
+    get_train_fn = fmp.readers.CaseDirect("train", "trainannot")
+    get_val_fn = fmp.readers.CaseDirect("val", "valannot")
+    class_values = [1, 8]
+    train_trans = albu.Compose(
+        [albu.augmentations.geometric.resize.Resize(256, 512),
+            albu.augmentations.transforms.HorizontalFlip(p=0.5)])
+    val_trans = albu.Compose(
+        [albu.augmentations.geometric.resize.Resize(256, 512)])
+
     batch_size = 16
     epochs = 30
     lr = 0.001
@@ -48,16 +53,14 @@ class OptimizationImp(fmp.GetOptimizationABC):
                     activation="softmax", in_channels=3, classes=2,)
     loss_func = smp.losses.DiceLoss('multilabel', from_logits=False)
 
-    @staticmethod
-    def scheduler_func(epoch):
-        return 0.95 ** (epoch-10) if epoch > 10 else 1
-
-
-def command():
-    train_anno, val_anno = AnnotationImp()()
-    train, val = DatasetImp(train_anno, training=True), DatasetImp(val_anno)
-    OptimizationImp(train, val)()
+    train_anno, val_anno = AnnotationImp(
+        target, get_train_fn=get_train_fn, get_val_fn=get_val_fn)()
+    train = DatasetImp(train_anno, class_values, train_trans)
+    val = DatasetImp(val_anno, class_values, val_trans)
+    OptimizationImp(
+        train, val,
+        batch_size, epochs, lr, gpus, optimizer_cls, model, loss_func)()
 
 
 if __name__ == "__main__":
-    command()
+    main()
